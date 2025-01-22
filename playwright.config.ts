21ff33c496ee0cfcf25a30e56222e7e0cb438d61
GerasimovEvgen <egerasimov@avanta.co>
2025-01-22 18:38:56 +0200

    jen

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : 3,
+  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['html']],
   expect: {
